f8c28d5e2ebf9955b440b8419414ce52e8fd1ae4
V-N-C <navinchandruv@gmail.com>
2024-02-24 22:26:50 +0530

    Annonymus functions answers added

--- Annonymus and IIFE.js
+++ Annonymus and IIFE.js
@@ -33,4 +33,40 @@ console.log(capitalizedNames);
 
 // Sum of all numbers in an array
 
+const sum = [1, 2, 3, 4].reduce((a, b) => a + b, 0);
+console.log(sum);
+
+// Return all the prime numbers in an array
+const navin = [4, 5, 6, 7].filter(num => num > 1 && Array.from({ length: Math.sqrt(num) }, (_, i) => i + 2).every(divisor => num % divisor !== 0));
+console.log(navin);
+
+// Return all the palindromes in an array
+const palindromes = ['racecar', 'cecarar', 'honda'].filter(word => word === word.split('').reverse().join(''));
+console.log(palindromes);
+
+// Return median of two sorted arrays of the same size
+const Array1 = [2, 4, 5];
+const Array2 = [6, 7, 9];
+const mergedArray = [...Array1, ...Array2].sort((a, b) => a - b);
+const n = mergedArray.length;
+const median = (mergedArray[Math.floor((n - 1) / 2)] + mergedArray[Math.ceil((n - 1) / 2)]) / 2;
+console.log(`Median: ${median}`);
+
+//Remove duplicates from an array
+const ArrayRem = [2, 2, 4, 6, 7, 5, 5];
+const uniqueArray = Array.from(new Set(ArrayRem));
+console.log(uniqueArray);
+
+//Rotate an array by k times
+const ArrayInitial = [1, 2, 3, 4];
+const k = 1;
+const rotatedArray = ArrayInitial.slice(k).concat(ArrayInitial.slice(0, k));
+console.log(rotatedArray); // Output: [2, 3, 4, 1]
+
+
+
+
+
+
+
   
